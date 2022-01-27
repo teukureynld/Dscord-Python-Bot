8e9a3edf7e00c48b61355fd3b988c0d36fa66968
TeukuReyn <91148295+Mystereyn@users.noreply.github.com>
2022-01-27 23:11:44 +0700

    Update Discord-Spam.py

--- discordSpammer/Discord-Spam.py
+++ discordSpammer/Discord-Spam.py
@@ -2,14 +2,14 @@ import discord
 
 Client = discord.Client()
 
-keywords = ["hello","who is me?"]
+keywords = ["hello"]
 
 @Client.event
 async def on_message(message):
     for i in range(len(keywords)):
         if keywords[i] in message.content:
             for j in range(25):
-                await message.channel.send('Hallo my Owner!',)
+                await message.channel.send('Hallo my Owner!)
 
-Client.run('OTM0Mzc1MjI3NjU0MzAzNzc1.YevKvg.oOGBaDGC4lIjBfWjozDPt4xjquQ')
+Client.run('tokenBotLo')
 
